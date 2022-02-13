73caf6bab6d553dd51c7183bbf7161dc8d1481ee
a-poor <a-poor@users.noreply.github.com>
2022-02-13 14:57:53 -0800

    Initial commit

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"html/template"
+	"fmt"
 	"os"
 	"strings"
+	"text/template"
+
+	"github.com/muesli/termenv"
 )
 
 type ListItem struct {
@@ -17,11 +20,21 @@ type List struct {
 	Items []ListItem
 }
 
-const tmpl = `Title: {{ .Title }}
+const titleTmpl = `{{define "title"}}Title: {{.Title}}{{end}}`
+
+const itemTmpl = `{{define "item" -}}
+[{{ if .Done }}x{{ else }} {{ end }}] {{ if .Active }}{{ .Name }}{{ else }}{{ Faint .Name }}{{ end }}
+{{- end}}`
+
+const listTmpl = `{{define "list"}}{{template "title" .}}
 {{- range .Items }}
-[{{ if .Done }}x{{ else }} {{ end }}] {{ if .Active }}{{ toUpper .Name }}{{ else }}{{ .Name }}{{ end }}
-{{- end }}
-`
+  {{template "item" .}}
+{{- else}}
+  (No items in list.)
+{{- end}}
+{{end}}`
+
+const tmpl = `{{ template "list" . }}`
 
 func main() {
 	// Create a list...
@@ -36,11 +49,30 @@ func main() {
 	}
 
 	// Parse the template & display...
-	t := template.New("list")
-	t.Funcs(template.FuncMap{
+	t := template.New("my-list")
+	t = t.Funcs(template.FuncMap{
 		"toUpper": strings.ToUpper,
 	})
-	t, err := t.Parse(tmpl)
+
+	// Add the termenv template functions
+	cp := termenv.ColorProfile()
+	fmt.Printf("Terminal color profile: %+v\n", cp)
+	f := termenv.TemplateFuncs(cp)
+	t = t.Funcs(f)
+
+	t, err := t.Parse(titleTmpl)
+	if err != nil {
+		panic(err)
+	}
+	t, err = t.Parse(itemTmpl)
+	if err != nil {
+		panic(err)
+	}
+	t, err = t.Parse(listTmpl)
+	if err != nil {
+		panic(err)
+	}
+	t, err = t.Parse(tmpl)
 	if err != nil {
 		panic(err)
 	}
